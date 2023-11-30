f877354cb79ed5fbd6ebd65d2894c1867ef189b0
Poonam Gahatraj <poonamgtrz@gmail.com>
2023-11-30 13:31:21 +0545

    class_practice

---
+++ class_practice/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+
+import firstpage from './components/firstpage.vue'
+</script>
+
+<template>
+  <firstpage/>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ class_practice/src/components/firstpage.vue
@@ -0,0 +1,25 @@
+<template>
+
+</template>
+
+<script>
+class RailwayForm{
+    submit(){
+            alert(this.name+" "+"your form is submitted")
+    }
+    cancel(){
+            alert(this.name+" "+"your form is  cancelled")
+    }
+    fill(givenname){
+            this.name=givenname
+    }
+}
+
+let x=new RailwayForm()
+x.fill("poonam")
+let y=new RailwayForm()
+y.fill("vishu")
+
+x.submit()
+y.cancel()
+</script>
